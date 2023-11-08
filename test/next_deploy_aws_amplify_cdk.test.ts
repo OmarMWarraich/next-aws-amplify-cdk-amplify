70a042756f78a97c4313ece6df63cc82442ea689
OmarMWarraich <earthling986@gmail.com>
2023-11-08 15:02:22 +0500

    Initial commit

---
+++ test/next_deploy_aws_amplify_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NextDeployAwsAmplifyCdk from '../lib/next_deploy_aws_amplify_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/next_deploy_aws_amplify_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NextDeployAwsAmplifyCdk.NextDeployAwsAmplifyCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
